e9e1f4d4dfea9d502cfb4221c43c37eb4beee39f
bpshift001@bancoctt.pt <bpshift001@bancoctt.pt>
2026-04-23 19:55:37 +0100

    Add initial test project and unit tests

---
+++ Tests/UnitTest1.cs
@@ -0,0 +1,30 @@
+﻿using System.Reflection;
+
+namespace Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void TestSucesso()
+    {
+        Assert.True(true, "teste ok");
+    }
+
+    [Fact]
+    public void TestSucesso2()
+    {
+        Assert.True(true, "teste ok");
+    }
+
+        [Fact]
+    public void TestSucesso3()
+    {
+        Assert.True(true, "teste ok");
+    }
+
+        [Fact]
+    public void TestFalha()
+    {
+        Assert.True(false, "teste com problemas");
+    }
+}
